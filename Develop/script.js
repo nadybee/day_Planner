751f4aaa414c1a9c298aae950301cfc62fb332c2
Nadybee <natalie@yoodlize.com>
2022-05-13 19:12:25 -0600

    added html loop to script, style fixes

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,14 @@
 // const moment = require('moment')
+const myHTML = []
+createHTML()
+$('.container').append(myHTML.join(''))
 const now = new Date($.now())
-const todayDate = moment(now).format('dddd, MMMM Do, ha') 
 
-// const currentHour = 10;
-const currentHour = moment(now).format('h')
+const todayDate = moment(now).format('dddd, MMMM Do, h:mm a z') 
+
+// const currentHour = 17;
+const currentHour = moment(now).format('H')
+
 $('#currentDay').append(todayDate);
 
 
@@ -11,21 +16,49 @@ console.log(parseInt(currentHour))
 
 
 
-$('.row').each( function(index, value){
-    console.log(this.id)
-    if(this.id==currentHour) {
+$('.middle-col').each( function(index, value){
+    // console.log(this.id)
+    if(parseInt(this.id)==currentHour) {
        $(this).addClass('present')
+       console.log(parseInt(this.id))
     }
-    else if (this.id < currentHour){
+    else if (parseInt(this.id) < currentHour){
         $(this).addClass('past')
     }
-    else if ((this.id) > currentHour){
+    else if (parseInt(this.id) > currentHour){
         $(this).addClass('future')
     }
 })
 
 
+function createHTML(){
+    let time=0;
+    let timeLetters;
+    for (let i=5; i<24; i++){
+        if(i>12){
+            time = i-12
+            timeLetters = 'pm'
+        }
+        else {
+            time = i
+            timeLetters = 'am'
+        }
+        myHTML.push(
+` <div class="row" >
+<div class="col-2 col-md-1 text-center hour">
+  ${time}${timeLetters}
+</div>
+<div class="middle-col col-8 col-md-10 description" id="${i}">
+  <div class="time-block">event</div>
+</div>
+<div class="col-2 col-md-1 saveBtn">
+  <i class="fa-solid fa-floppy-disk p-1 text-white"></i>
+</div>
+</div>`
 
-// $( function() {
-//     $('#datepicker').datepicker()
-// })
+        )
+    }
+    return myHTML
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## Module_5 assignment
+
+jquery wigets ui
+jquery qigets sortable lists
+moment js 
