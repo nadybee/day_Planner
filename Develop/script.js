9d846be66017a355e652caabfa087ca81ddc6978
Nadybee <natalie@yoodlize.com>
2022-05-15 20:20:01 -0600

    local storage function working

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-// const moment = require('moment')
+
 const myHTML = []
 createHTML()
 $('.container').append(myHTML.join(''))
@@ -16,7 +16,7 @@ console.log(parseInt(currentHour))
 
 
 
-$('.middle-col').each( function(index, value){
+$('.row').each( function(index, value){
     // console.log(this.id)
     if(parseInt(this.id)==currentHour) {
        $(this).addClass('present')
@@ -38,19 +38,23 @@ function createHTML(){
         if(i>12){
             time = i-12
             timeLetters = 'pm'
+            
+        }
+        else if (i===12){
+            time = 12
+            timeLetters = 'pm'
         }
         else {
             time = i
             timeLetters = 'am'
         }
         myHTML.push(
-` <div class="row" >
+` <div class="row time-block" id="${i}" >
 <div class="col-2 col-md-1 text-center hour">
   ${time}${timeLetters}
 </div>
-<div class="middle-col col-8 col-md-10 description" id="${i}">
-  <div class="time-block">event</div>
-</div>
+<textarea class="middle-col col-8 col-md-10 description" >
+</textarea>
 <div class="col-2 col-md-1 saveBtn">
   <i class="fa-solid fa-floppy-disk p-1 text-white"></i>
 </div>
@@ -62,3 +66,20 @@ function createHTML(){
 
 }
 
+
+    $('.saveBtn').on('click', function(){
+        var event = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, event);
+        
+        $('header').after(`${event} was added!`)
+
+    })
+
+const renderEvents = () => {
+     for (let i=5; i<24; i++){
+         $(`#${i} .description`).val(localStorage.getItem(`${i}`))
+}
+console.log('rendered')
+}
+renderEvents()
