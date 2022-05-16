f9da67fa31e0dff131ac19329b9679c7e2778d18
Nadybee <natalie@yoodlize.com>
2022-05-16 08:13:39 -0600

    edit readme and organized code

--- Develop/script.js
+++ Develop/script.js
@@ -1,55 +1,28 @@
-
-const myHTML = []
-createHTML()
-$('.container').append(myHTML.join(''))
+/** gets current date and time and displays it on page */
 const now = new Date($.now())
+const todayDate = moment(now).format("dddd, MMMM Do, h:mm a z")
+const currentHour = moment(now).format("H")
+$("#currentDay").append(todayDate)
 
-const todayDate = moment(now).format('dddd, MMMM Do, h:mm a z') 
-
-// const currentHour = 17;
-const currentHour = moment(now).format('H')
-
-$('#currentDay').append(todayDate);
-
-
-console.log(parseInt(currentHour))
-
-
-
-$('.row').each( function(index, value){
-    // console.log(this.id)
-    if(parseInt(this.id)==currentHour) {
-       $(this).addClass('present')
-       console.log(parseInt(this.id))
+/** creates html to show time rows */
+const myHTML = []
+/** function to write the html */
+function createHTML() {
+  let time = 0
+  let timeLetters
+  for (let i = 5; i < 24; i++) {
+    if (i > 12) {
+      time = i - 12
+      timeLetters = "pm"
+    } else if (i === 12) {
+      time = 12
+      timeLetters = "pm"
+    } else {
+      time = i
+      timeLetters = "am"
     }
-    else if (parseInt(this.id) < currentHour){
-        $(this).addClass('past')
-    }
-    else if (parseInt(this.id) > currentHour){
-        $(this).addClass('future')
-    }
-})
-
-
-function createHTML(){
-    let time=0;
-    let timeLetters;
-    for (let i=5; i<24; i++){
-        if(i>12){
-            time = i-12
-            timeLetters = 'pm'
-            
-        }
-        else if (i===12){
-            time = 12
-            timeLetters = 'pm'
-        }
-        else {
-            time = i
-            timeLetters = 'am'
-        }
-        myHTML.push(
-` <div class="row time-block" id="${i}" >
+    myHTML.push(
+      ` <div class="row time-block" id="${i}" >
 <div class="col-2 col-md-1 text-center hour">
   ${time}${timeLetters}
 </div>
@@ -59,27 +32,42 @@ function createHTML(){
   <i class="fa-solid fa-floppy-disk p-1 text-white"></i>
 </div>
 </div>`
-
-        )
-    }
-    return myHTML
-
+    )
+  }
+  return myHTML
 }
 
+createHTML()
+/** adds the html to page */
+$(".container").append(myHTML.join(""))
 
-    $('.saveBtn').on('click', function(){
-        var event = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-        localStorage.setItem(time, event);
-        
-        $('header').after(`${event} was added!`)
+/** adds conditional styling to rows */
+$(".row").each(function (index, value) {
+  // console.log(this.id)
+  if (parseInt(this.id) == currentHour) {
+    $(this).addClass("present")
+    console.log(parseInt(this.id))
+  } else if (parseInt(this.id) < currentHour) {
+    $(this).addClass("past")
+  } else if (parseInt(this.id) > currentHour) {
+    $(this).addClass("future")
+  }
+})
 
-    })
+/** saves users input event to local storage */
+$(".saveBtn").on("click", function () {
+  var event = $(this).siblings(".description").val()
+  var time = $(this).parent().attr("id")
+  localStorage.setItem(time, event)
 
+  $("#event-notification").text(`"${event}" was added to local storage ✅`)
+})
+
+/** gets users events from local storage to display */
 const renderEvents = () => {
-     for (let i=5; i<24; i++){
-         $(`#${i} .description`).val(localStorage.getItem(`${i}`))
-}
-console.log('rendered')
+  for (let i = 5; i < 24; i++) {
+    $(`#${i} .description`).val(localStorage.getItem(`${i}`))
+  }
+
 }
 renderEvents()
--- README.md
+++ README.md
@@ -1,5 +1,31 @@
-## Module_5 assignment
+# Module 5 assignment
+## Work Day Scheduler
+### module 5 challenge for bootcamp
 
-jquery wigets ui
-jquery qigets sortable lists
-moment js 
+This app is built using:
+*   HTML/CSS
+
+*   Javascript
+
+*   Jquery
+
+*   Bootstrap
+
+*   Moment.js
+
+I used javascript and jquery to write most of the html. I use bootstrap columns to create the structure. I used Moment.js to format that date and time.
+
+Live app deployment can be found here:
+
+https://nadybee.github.io/module_5/
+
+I was given starter code that can be found here:
+
+https://github.com/coding-boot-camp/super-disco
+
+![alt text](Develop/assets/Screen%20Shot%202022-05-16%20at%208.09.57%20AM.png)
+
+
+
+
+ 
